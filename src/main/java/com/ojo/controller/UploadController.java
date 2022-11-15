bf6928402c6272182fbb9f364f0abffe89b37c5b
srkim <ksr916@hanmail.net>
2022-11-15 18:24:31 +0900

    수정

---
+++ src/main/java/com/ojo/controller/UploadController.java
@@ -0,0 +1,67 @@
+package com.ojo.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UploadController")
+public class UploadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UploadController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		String filename = request.getParameter("file"); // 업로드한 파일 이름
+		String fileRealname = request.getParameter("realfile"); // 실제 디스크에 업로드된 파일 이름
+		
+		String uploadDirectory = "E:/uploadImage/" + fileRealname;
+		File file = new File(uploadDirectory);
+		
+		String mimeType = getServletContext().getMimeType(file.toString());
+		if (mimeType == null) {
+			response.setContentType("application/octet-stream");
+		}
+		
+		String downloadName = null;
+		if (request.getHeader("user-agent").indexOf("MSIE") == -1) {
+			downloadName = new String(filename.getBytes("UTF-8"), "8859_1");
+		} else {
+			downloadName = new String(filename.getBytes("EUC-KR"), "8859_1");
+		}
+		response.setHeader("Content-Disposition", "attachment;filename=\"" + downloadName + "\";");
+		
+		FileInputStream fileInputStream = new FileInputStream(file);
+		ServletOutputStream outputStream = response.getOutputStream();
+		byte[] b = new byte[1024];
+		int data = 0;
+		
+		while ((data = fileInputStream.read(b, 0, b.length)) != -1) {
+			outputStream.write(b, 0, data);
+		}
+		
+		outputStream.flush();
+		outputStream.close();
+		fileInputStream.close();
+	}
+
+}
